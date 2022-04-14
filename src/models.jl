9f93bc3914820cb257bacd38bdde713e30dfadc4
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-04-14 10:52:34 +0800

    code clean

--- src/models.jl
+++ src/models.jl
@@ -15,11 +15,14 @@ hil(x, k, n) := \frac{x^n}{x^n + k^n}
 hil(x, k) = x / (x + k)
 hil(x, k, n) = hil(x^n, k^n)
 
+"""Constant damage signal"""
+CONSTANT_SIGNAL(s) = t -> s
+
 """Damage signal = 0"""
-ZERO_SIGNAL(t) = 0
+ZERO_SIGNAL = CONSTANT_SIGNAL(0)
 
 """Damage signal = 1"""
-ONE_SIGNAL(t) = 1
+ONE_SIGNAL = CONSTANT_SIGNAL(1)
 
 # Stressed protein levels
 const STRESSED = Dict(
@@ -50,66 +53,55 @@ function RtgMTK(S=ZERO_SIGNAL; name,
     proteinlevels=STRESSED, simplify=true, recordNruns=false)
 
     D = Differential(t)
-
-    # Modulation Layer
-    @variables v_01(t) v_02(t) v_03(t)
-
-    # Nuclear Localization Signal (NLS) activation
-    @variables v_04(t) v_05(t) v_06(t)
-
-    # Rtg1Binding: Rtg13 formation
-    @variables v_07(t) v_08(t) v_09(t) v_10(t)
-
-    # Translocation
-    @variables v_11(t) v_12(t) v_13(t)
+    @variables v[1:13](t)
 
     # MTK does not support elimination in overdetermined systems
     # ODEs are eliminated manually
     eqs = [
         s ~ S(t),
         # Rtg2I_c <=> Rtg2A_c
-        v_01 ~ ksV * hil(s * mul_S, ksD, n_S) * Rtg2I_c - k2I * Rtg2A_c,
+        v[1] ~ ksV * hil(s * mul_S, ksD, n_S) * Rtg2I_c - k2I * Rtg2A_c,
         # Rtg2A_c + Mks <=> Rtg2Mks_c
-        v_02 ~ k2M * Rtg2A_c * Mks - kn2M * Rtg2Mks_c,
+        v[2] ~ k2M * Rtg2A_c * Mks - kn2M * Rtg2Mks_c,
         # Bmh + Mks <=> BmhMks
-        v_03 ~ kBM * Bmh * Mks - knBM * BmhMks,
+        v[3] ~ kBM * Bmh * Mks - knBM * BmhMks,
         # Rtg13A_c => Rtg13I_c
-        v_04 ~ (k13I + k13IV * hil(BmhMks, k13ID)) * Rtg13A_c,
+        v[4] ~ (k13I + k13IV * hil(BmhMks, k13ID)) * Rtg13A_c,
         # Rtg3I_c <=> Rtg3A_c
-        v_05 ~ k3A_c * Rtg3I_c - k3I_c * Rtg3A_c,
+        v[5] ~ k3A_c * Rtg3I_c - k3I_c * Rtg3A_c,
         # Rtg3A_n => Rtg3I_n
-        v_06 ~ k3I_n * Rtg3A_n,
+        v[6] ~ k3I_n * Rtg3A_n,
         # Rtg1_c + Rtg3A_c <=> Rtg13A_c
-        v_07 ~ k13_c * Rtg1_c * Rtg3A_c - kn13_c * Rtg13A_c,
+        v[7] ~ k13_c * Rtg1_c * Rtg3A_c - kn13_c * Rtg13A_c,
         # Rtg1_c + Rtg3I_c <=> Rtg13I_c
-        v_08 ~ k13_c * Rtg1_c * Rtg3I_c - kn13_c * Rtg13I_c,
+        v[8] ~ k13_c * Rtg1_c * Rtg3I_c - kn13_c * Rtg13I_c,
         # Rtg1_n + Rtg3A_n <=> Rtg13A_n
-        v_09 ~ k13_n * Rtg1_n * Rtg3A_n - kn13_n * Rtg13A_n,
+        v[9] ~ k13_n * Rtg1_n * Rtg3A_n - kn13_n * Rtg13A_n,
         # Rtg1_n + Rtg3I_n <=> Rtg13I_n
-        v_10 ~ k13_n * Rtg1_n * Rtg3I_n - kn13_n * Rtg13I_n,
+        v[10] ~ k13_n * Rtg1_n * Rtg3I_n - kn13_n * Rtg13I_n,
         # Rtg1_c <=> Rtg1_n
-        v_11 ~ k1in * Rtg1_c - k1out * Rtg1_n,
+        v[11] ~ k1in * Rtg1_c - k1out * Rtg1_n,
         # Rtg3A_c <=> Rtg3A_n
-        v_12 ~ k3inA * Rtg3A_c - k3outA * Rtg3A_n,
+        v[12] ~ k3inA * Rtg3A_c - k3outA * Rtg3A_n,
         # Rtg3I_c <=> Rtg3I_n
-        v_13 ~ k3inI * Rtg3I_c - k3outI * Rtg3I_n,
+        v[13] ~ k3inI * Rtg3I_c - k3outI * Rtg3I_n,
 
-        # D(Rtg2I_c) ~ -v_01,
-        D(Rtg2A_c) ~ v_01 - v_02,
-        # D(Mks) ~ -v_02 - v_03,
-        D(Rtg2Mks_c) ~ v_02,
-        # D(Bmh) ~ -v_03,
-        D(BmhMks) ~ v_03,
-        D(Rtg13A_c) ~ -v_04 + v_07,
-        D(Rtg13I_c) ~ v_04 + v_08,
-        # D(Rtg3I_c) ~ -v_05 - v_08 - v_13,
-        D(Rtg3A_c) ~ v_05 - v_07 - v_12,
-        D(Rtg3A_n) ~ -v_06 - v_09 + v_12,
-        D(Rtg3I_n) ~ v_06 - v_10 + v_13,
-        # D(Rtg1_c) ~ -v_07 - v_08 - v_11,
-        D(Rtg1_n) ~ -v_09 - v_10 + v_11,
-        D(Rtg13A_n) ~ v_09,
-        D(Rtg13I_n) ~ v_10,
+        # D(Rtg2I_c) ~ -v[1],
+        D(Rtg2A_c) ~ v[1] - v[2],
+        # D(Mks) ~ -v[2] - v[3],
+        D(Rtg2Mks_c) ~ v[2],
+        # D(Bmh) ~ -v[3],
+        D(BmhMks) ~ v[3],
+        D(Rtg13A_c) ~ -v[4] + v[7],
+        D(Rtg13I_c) ~ v[4] + v[8],
+        # D(Rtg3I_c) ~ -v[5] - v[8] - v[13],
+        D(Rtg3A_c) ~ v[5] - v[7] - v[12],
+        D(Rtg3A_n) ~ -v[6] - v[9] + v[12],
+        D(Rtg3I_n) ~ v[6] - v[10] + v[13],
+        # D(Rtg1_c) ~ -v[7] - v[8] - v[11],
+        D(Rtg1_n) ~ -v[9] - v[10] + v[11],
+        D(Rtg13A_n) ~ v[9],
+        D(Rtg13I_n) ~ v[10],
 
         # Conservation relationships
         ΣRtg1 ~ Rtg1_c + Rtg1_n + Rtg13A_c + Rtg13I_c + Rtg13A_n + Rtg13I_n,
