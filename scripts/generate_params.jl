5e3eaab95a5eff70bb38d180bfd05974ce0ef533
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-04-25 08:22:35 +0800

    generate more params

--- scripts/generate_params.jl
+++ scripts/generate_params.jl
@@ -4,18 +4,14 @@ using DataFrames
 using Optim
 using CSV
 
-optimoptions = Optim.Options(iterations=10^5, show_trace=true, show_every=1000)
+versioninfo()
 
-parammaps = map(1:10) do iter
-    @show iter
-    _, parammap = optim_params(; optimoptions)
-    parammap
+optimoptions = Optim.Options(iterations=10^5)
+
+res = optim_params_threads(120)
+
+parammaps = map(res) do x
+    Dict(Symbol(k) => v for (k, v) in x.parammap)
 end
 
-parammapssymbol = map(parammaps) do x
-    Dict(Symbol(k) => v for (k, v) in x)
-end
-
-df = DataFrame(parammapssymbol)
-
-CSV.write("solution_rtgMTK_optim.csv", df)
+CSV.write("solution_rtgMTK_optim.csv", DataFrame(parammaps))
