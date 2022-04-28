4469a08795309035ba3853ad3ccd8ee3a670796e
Wen Wei Tseng <sosiristseng@gmail.com>
2022-04-28 16:59:40 +0800

    Fixes #23 (#24)

--- test/steadystates.jl
+++ test/steadystates.jl
@@ -1,7 +1,9 @@
 using SciMLBase
+using SteadyStateDiffEq
 
 # Test find_steady_states
 res = find_steady_states(trajectories=10)
 
 @test res isa EnsembleSolution
+@test res[1] isa SteadyStateSolution
 @test length(res) > 0
